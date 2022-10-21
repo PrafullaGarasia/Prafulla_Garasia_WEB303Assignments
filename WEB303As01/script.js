536b0bf10a5d82e94234ef188572b362cb1a5f04
PrafullaGarasia <100746487+PrafullaGarasia@users.noreply.github.com>
2022-10-21 17:17:26 -0400

    update

--- WEB303As01/script.js
+++ WEB303As01/script.js
@@ -3,28 +3,27 @@
 	{Prafulla Kalusinh Garasia}
 */
 
-$(document).ready(function() 
-{
-    console.log("ready!");
+$(document).ready(function () {
+	console.log("ready!");
 
-     $("input").keyup(function()
-	 {
-    
-		var textbox_Salary = $('#yearly-salary').val(); 
-		
-		var textbox_Salary = $('#percent').val(); 
+	$("input").keyup(function () {
 
-		console.log("Sal==",textbox_Salary);
+		let Salary = $('#yearly-salary').val();
 
-		var amount_cal = (textbox_Salary * textbox_Salary) / 100;
-        
+		let Percent = $('#percent').val();
+
+		console.log("EnterSalary==", Salary);
+		console.log("EnterPercent==", Percent);
+
+		let amount_cal = (Salary * Percent) / 100;
+
 		console.log(amount_cal.toFixed(2));
-  
+
 		$("#amount_cal").val("Result :" + amount_cal.toFixed(2));
-  
-		$('#amount').html("$"+amount_cal.toFixed(2));
 
-     console.log("cal_Value==",amount_cal.toFixed(2));
+		$('#amount').html("$" + amount_cal.toFixed(2));
 
-    });
+		console.log("cal_Value==", amount_cal.toFixed(2));
+
+	});
 });
